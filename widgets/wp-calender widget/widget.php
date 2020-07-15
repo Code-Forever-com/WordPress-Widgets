e3bca255bdcb1df710aeb460f9da32ac84f0ab29
Swindler36 <furkanzerman@gmail.com>
2020-07-15 20:49:54 +0300

    adding calender widget

---
+++ widgets/wp-calender widget/widget.php
@@ -0,0 +1,66 @@
+<div class="card" id="takvim">
+    <div class="card-header"><span id="month_name" class="mr-3"></span>
+        <span class="float-right">
+            <a class="button btn-info" href="" id="month_up"><i class="fa fa-angle-up"></i></a>
+            <a class="button btn-info" href="" id="month_down"><i class="fa fa-angle-down"></i></a>
+        </span>
+    </div>
+    <table class="tbl mb-0">
+        <thead>
+            <th scope="col">Mo</th>
+            <th scope="col">Tu</th>
+            <th scope="col">We</th>
+            <th scope="col">Tu</th>
+            <th scope="col">Fr</th>
+            <th scope="col">Sa</th>
+            <th scope="col">Su</th>
+        </thead>
+        <tbody>
+            <tr id="week1">
+                <td class="day"></td>
+                <td class="day"></td>
+                <td class="day"></td>
+                <td class="day"></td>
+                <td class="day"></td>
+                <td class="day"></td>
+                <td class="day"></td>
+            </tr>
+            <tr id="week2">
+                <td class="day"></td>
+                <td class="day"></td>
+                <td class="day"></td>
+                <td class="day"></td>
+                <td class="day"></td>
+                <td class="day"></td>
+                <td class="day"></td>
+            </tr>
+            <tr id="week3">
+                <td class="day"></td>
+                <td class="day"></td>
+                <td class="day"></td>
+                <td class="day"></td>
+                <td class="day"></td>
+                <td class="day"></td>
+                <td class="day"></td>
+            </tr>
+            <tr id="week4">
+                <td class="day"></td>
+                <td class="day"></td>
+                <td class="day"></td>
+                <td class="day"></td>
+                <td class="day"></td>
+                <td class="day"></td>
+                <td class="day"></td>
+            </tr>
+            <tr id="week5">
+                <td class="day"></td>
+                <td class="day"></td>
+                <td class="day"></td>
+                <td class="day"></td>
+                <td class="day"></td>
+                <td class="day"></td>
+                <td class="day"></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
